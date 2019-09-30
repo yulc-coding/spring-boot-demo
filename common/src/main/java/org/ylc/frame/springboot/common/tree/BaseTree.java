6a5eb7a5afd4ad54244676f6a6c60f51e394342c
superylcfly <350009335@qq.com>
2019-09-30 16:40:36 +0800

    基本参数

---
+++ common/src/main/java/org/ylc/frame/springboot/common/tree/BaseTree.java
@@ -0,0 +1,42 @@
+package org.ylc.frame.springboot.common.tree;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 代码千万行，注释第一行，
+ * 注释不规范，同事泪两行。
+ * <p>
+ * 基础树结构
+ *
+ * @author yulc
+ * @version 1.0.0
+ * @date 2019/9/30
+ */
+@Data
+public class BaseTree<T> implements Serializable {
+
+    /**
+     * id
+     */
+    private String id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 父id
+     */
+    private String pid;
+
+    /**
+     * 子类
+     */
+    private List<T> children;
+
+
+}
