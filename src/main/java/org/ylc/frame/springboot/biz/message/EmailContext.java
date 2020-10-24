4f249711dcd37b84a585f95c638e8b4e68c6606f
Yulc <350009335@qq.com>
2020-10-24 20:55:06 +0800

    connect test

---
+++ src/main/java/org/ylc/frame/springboot/biz/message/EmailContext.java
@@ -0,0 +1,15 @@
+package org.ylc.frame.springboot.biz.message;
+
+/**
+ * 代码全万行，注释第一行
+ * 注释不规范，同事泪两行
+ * <p>
+ * 邮件相关
+ *
+ * @author YuLc
+ * @version 1.0.0
+ * @date 2020-10-24
+ */
+public class EmailContext {
+
+}
